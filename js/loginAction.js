ade228184821492d4b42cbf59b559b22c1ff6943
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-09-23 05:37:58 +1000

    minor changes

--- js/loginAction.js
+++ js/loginAction.js
@@ -1,10 +1,24 @@
 var id = document.getElementById("id_tf");
+var password = document.getElementById("password_tf");
 var login_btn = document.getElementById("pLogin_btn");
 
 function loginClicked(){
-    loginValidation();
-    window.alert(id.value);
+    if(loginValidation()){
+        window.alert(id.value+" clicked the button\n"+"password is "+password.value);
+    }
+    else{
+        window.alert("wrong input");
+    }
 }
+
 function loginValidation(){
-    
+    if(id.value.length!=8){
+        return false;
+    }
+    else if(password.value ==""){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
