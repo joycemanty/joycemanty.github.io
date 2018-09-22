e307c0548b6684abcd36493b8cd058ab903025b9
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-09-23 04:50:09 +1000

    firebase initialised

---
+++ js/registration.js
@@ -0,0 +1,12 @@
+var firebase = require("firebase");
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyDFuBAb7gmrOzAzhkkpAxphBszEr5O0l_k",
+    authDomain: "sep-2018-f456e.firebaseapp.com",
+    databaseURL: "https://sep-2018-f456e.firebaseio.com",
+    storageBucket:  "sep-2018-f456e.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+ window.alert("hi");
