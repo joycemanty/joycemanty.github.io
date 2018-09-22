4996d68a33f4ae7e7e376e22d5847a2a05a9f282
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-09-23 05:19:10 +1000

    html connected to java successfully, check patient login for example

---
+++ js/loginAction.js
@@ -0,0 +1,10 @@
+var id = document.getElementById("id_tf");
+var login_btn = document.getElementById("pLogin_btn");
+
+function loginClicked(){
+    loginValidation();
+    window.alert(id.value);
+}
+function loginValidation(){
+    
+}
--- js/registration.js
+++ js/registration.js
@@ -9,4 +9,5 @@ var config = {
   };
   firebase.initializeApp(config);
 
- window.alert("hi");
+
+  
