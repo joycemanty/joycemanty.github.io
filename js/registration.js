2d2ecfaee760e748e41154f3d3754679de526d45
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-09-23 16:28:58 +1000

    registration form saving successful

--- js/registration.js
+++ js/registration.js
@@ -8,26 +8,65 @@ var config = {
   messagingSenderId: "395954175951"
   };
 firebase.initializeApp(config);
-console.log(firebase);
+console.log(firebase); // testing use
 var database = firebase.database();
 var ref = database.ref('Users');
 //End of setup
 
-//Initailise Fields
-var fName;
-var lName;
-var utsId;
-var password;
-var email;
-var contact_num;
+ //Initailise Fields
+ var fName = document.getElementById("fName");
+ var lName = document.getElementById("lName");
+ var utsId = document.getElementById("utsId");
+ var password = document.getElementById("password");
+ var email = document.getElementById("email");
+ var contact_num = document.getElementById("cNumber");
+ var address = document.getElementById("address");
+ var allergies = document.getElementsByName("allergiesRadio");//note
+ var current_meditation = document.getElementById("current_medication");
+ var family_history; //note
 
 
+function getAllergiesValue(allergiesRadios)
+{
+   var allergiesRadio = document.forms[0].elements[allergiesRadios]
+ console.log(allergiesRadios.length);
+   for(var i = 0; i < allergiesRadio.length; i++)
+   {
+      if(allergiesRadio[i].checked)
+      {
+         return allergiesRadio[i].value;
+      }
+   }
+   return '';
+}
+
+function getFamiliyHistory(){
+  var checkedValue = ""; 
+  var inputElements = document.getElementsByClassName('checkbox_grp');
+  for(var i=0; inputElements[i]; ++i){
+        if(inputElements[i].checked){
+          checkedValue = checkedValue + inputElements[i].value + ", ";
+     }
+  }
+  return checkedValue;
+}
 
 function submitClicked(){
+ 
+  //start Saving
   var data = {
-    UTS_ID: "12345678",
-    First_Name: "Joyce"
+    UTS_ID: utsId.value,
+    First_Name: fName.value,
+    Last_Name: lName.value,
+    password: password.value,
+    Email: email.value,
+    Contact_Number: contact_num.value,
+    Address: address.value,
+    //Allergies: getAllergiesValue(allergies) <<< Not success yet
+    Current_Medication: current_meditation.value,
+    Familiy_History: getFamiliyHistory(),
   }
 ref.push(data);
 }
+
 
