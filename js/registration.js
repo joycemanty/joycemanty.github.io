f22b9fdf3d5d31ad5ef6e825f4ab5ae47db0536c
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-09-23 14:57:49 +1000

    saving data success

--- js/registration.js
+++ js/registration.js
@@ -1,13 +1,33 @@
-var firebase = require("firebase");
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
+//Setting up firebase
 var config = {
-    apiKey: "AIzaSyDFuBAb7gmrOzAzhkkpAxphBszEr5O0l_k",
-    authDomain: "sep-2018-f456e.firebaseapp.com",
-    databaseURL: "https://sep-2018-f456e.firebaseio.com",
-    storageBucket:  "sep-2018-f456e.appspot.com",
+  apiKey: "AIzaSyDFuBAb7gmrOzAzhkkpAxphBszEr5O0l_k",
+  authDomain: "sep-2018-f456e.firebaseapp.com",
+  databaseURL: "https://sep-2018-f456e.firebaseio.com",
+  projectId: "sep-2018-f456e",
+  storageBucket: "sep-2018-f456e.appspot.com",
+  messagingSenderId: "395954175951"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+console.log(firebase);
+var database = firebase.database();
+var ref = database.ref('Users');
+//End of setup
+
+//Initailise Fields
+var fName;
+var lName;
+var utsId;
+var password;
+var email;
+var contact_num;
 
 
-  
+
+function submitClicked(){
+  var data = {
+    UTS_ID: "12345678",
+    First_Name: "Joyce"
+  }
+ref.push(data);
+}
+
