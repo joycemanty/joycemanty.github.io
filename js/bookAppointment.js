ae3f65109ea5475a2d4efdfd26fd5229d77efdd3
Joyce Tin Yan MAN <12499947@stuent.uts.edu.au>
2018-10-03 11:46:37 +1000

    setting user id in every html for patient

--- js/bookAppointment.js
+++ js/bookAppointment.js
@@ -13,6 +13,24 @@ var database = firebase.database();
 var ref = database.ref('Appointments');
 
 var f_name,l_name,Id,date,time,a_type,doctor;
+setNav();
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+function setNav(){
+    document.getElementById("home_nav").setAttribute("href", "../http/patientHome.html?id="+getQueryVariable("id"));
+    document.getElementById("book_nav").setAttribute("href", "../http/bookAppointment.html?id="+getQueryVariable("id"));
+    document.getElementById("view_nav").setAttribute("href", "../http/patientAppointments.html?id="+getQueryVariable("id"));
+}
 
 
 function onBookClicked(){
--- js/patientsAppointment.js
+++ js/patientsAppointment.js
@@ -1,7 +1,3 @@
-var parameters = location.search.substring(1).split("&");
-var temp = parameters[0].split("=");
-x = unescape(temp[1]);
-document.getElementById('idReceived').innerHTML="Welcome back "+x+"! Would you like to book an appointmentment?";
 setNav();
 
 function getQueryVariable(variable)
