be35d5b8a607da20757cb896f2828c61c5b462ee
fancyyyyyyy <12643161@student.uts.edu.au>
2018-10-12 23:53:08 +1100

    1111

--- js/staffHome.js
+++
@@ -1,4 +0,0 @@
-var parameters = location.search.substring(1).split("&");
-var temp = parameters[0].split("=");
-x = unescape(temp[1]);
-document.getElementById('idReceived').innerHTML="Welcome back,"+x+" !";
